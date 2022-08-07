874dbcbd5f0c0459f431ff27d058c20a95deb6d9
김벽걸 <biob1121@naver.com>
2022-08-07 15:18:24 +0900

    feat: PlusData 수정

--- soket-study/src/components/SoketPage.tsx
+++ soket-study/src/components/SoketPage.tsx
@@ -2,28 +2,34 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import styled from "styled-components";
 
+interface itemType {
+  id: string;
+  text?: string;
+}
+
 const SoketPage = () => {
   const socket = io("http://localhost:5000/", { transports: ["websocket"] });
 
-  const [text, setText] = useState<string>("");
-  const [list, setList] = useState<string[]>([]);
+  const [text, setText] = useState<string>();
+  const [item, setItem] = useState<itemType[]>([]);
 
   const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
   const postText = () => {
-    socket.emit("list", {
-      data: text,
+    let emitItem: itemType = {
+      id: `${item.length - 1}`,
+      text: text,
+    };
+    socket.emit("plus", {
+      data: emitItem,
     });
+    setText("");
   };
 
-  const listInText = () => {
-    let copyList = [...list];
-    copyList.push(text);
-    setList(copyList);
-    setText("");
-    postText();
+  const deleteText = () => {
+    socket.emit("delete", {});
   };
 
   useEffect(() => {
@@ -35,16 +41,32 @@ const SoketPage = () => {
       <h1>소켓 통신 테스트</h1>
       <_DisplayFlex>
         <_SoketPageInput onChange={onChangeText} value={text} />
-        <_SoketPagePlusButton onClick={listInText}>추가</_SoketPagePlusButton>
+        <_SoketPagePlusButton onClick={postText}>추가</_SoketPagePlusButton>
       </_DisplayFlex>
       <ul>
-        {list.map((item: string, idx: number) => {
-          return <li key={idx}>{item}</li>;
+        {item.map((item: itemType, idx: number) => {
+          return (
+            <_ListLayout key={idx}>
+              <span>{item.text}</span>
+              <_DeleteButton name={item.id}>삭제</_DeleteButton>
+            </_ListLayout>
+          );
         })}
       </ul>
     </_SoketPageContainer>
   );
 };
+
+const _DeleteButton = styled.button`
+  width: 100px;
+  height: 100%;
+`;
+
+const _ListLayout = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
 
 const _SoketPageContainer = styled.div`
   display: flex;
